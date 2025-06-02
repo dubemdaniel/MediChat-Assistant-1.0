84cc68ee9fddb763815e9313395c1b60a6e46655
Daniel Chukwudubem <princedaniel7890@gmail.com>
2025-06-02 17:50:56 +0100

    improved the flow generally, added prescription and treatment flow, etc 1.1

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -2,7 +2,7 @@ import ChatInterface from "@/components/chat/chat-interface";
 
 export default function ChatPage() {
   return (
-    <div className="flex flex-col h-[calc(100vh-4rem)] md:h-[calc(100vh-10rem)] -mx-2">
+    <div className="flex flex-col h-[calc(100dvh-8rem)] md:h-[calc(100vh-10rem)] -mx-2">
       <ChatInterface />
     </div>
   );
